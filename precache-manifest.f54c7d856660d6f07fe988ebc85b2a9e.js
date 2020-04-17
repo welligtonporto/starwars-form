326c418c14bfbecf9760670edc8b042516b5abf2
Welligton <welligtonporto@gmail.com>
2020-04-17 14:03:41 -0300

    Updates

---
+++ precache-manifest.f54c7d856660d6f07fe988ebc85b2a9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068a06ff33fc6739702fedcf48945d0a",
+    "url": "/starwars-form/index.html"
+  },
+  {
+    "revision": "84a97dc33e00832f3752",
+    "url": "/starwars-form/static/css/main.922377a1.chunk.css"
+  },
+  {
+    "revision": "21dcf805e600abafe45a",
+    "url": "/starwars-form/static/js/2.6577ea5d.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/starwars-form/static/js/2.6577ea5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a97dc33e00832f3752",
+    "url": "/starwars-form/static/js/main.66465d71.chunk.js"
+  },
+  {
+    "revision": "7f0109a86cdfd6fa9089",
+    "url": "/starwars-form/static/js/runtime-main.61b2778b.js"
+  },
+  {
+    "revision": "9fbe321575dc930b1153d32030b6d5fc",
+    "url": "/starwars-form/static/media/empire.9fbe3215.svg"
+  },
+  {
+    "revision": "974cc90a3c57def56fc46610a0e8828d",
+    "url": "/starwars-form/static/media/kenobi.974cc90a.svg"
+  },
+  {
+    "revision": "0bd04385acd6fa81c745b23d6dc03c50",
+    "url": "/starwars-form/static/media/maul.0bd04385.svg"
+  },
+  {
+    "revision": "7b435b3c4366a956f3ed6d4df116f844",
+    "url": "/starwars-form/static/media/skywalker.7b435b3c.svg"
+  },
+  {
+    "revision": "be343f8e3011661f3bb2fb763dbf56ce",
+    "url": "/starwars-form/static/media/starwars-logo.be343f8e.svg"
+  },
+  {
+    "revision": "dbc8903dc8e26053b2036c05363a3c4b",
+    "url": "/starwars-form/static/media/vader.dbc8903d.svg"
+  },
+  {
+    "revision": "0b67eeccc58475fdd2f9d0d22dd7c48c",
+    "url": "/starwars-form/static/media/yoda.0b67eecc.svg"
+  }
+]);
